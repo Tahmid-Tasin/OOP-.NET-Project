705ad2c34dd2538eb98b70c84f1c4f3d80384706
Nawar704 <nawaradiba704@gmail.com>
2025-08-29 16:08:13 +0600

    Fixed table UI layout of employee

--- Store/Store/userinterface/EmployeeManage.Designer.cs
+++ Store/Store/userinterface/EmployeeManage.Designer.cs
@@ -83,14 +83,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ID_Coloumn,
-            this.Name_Coloumn,
-            this.Mobile_Coloumn,
-            this.Address_Coloumn});
-            this.dataGridView1.Location = new System.Drawing.Point(78, 60);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.ID_Coloumn, this.Name_Coloumn, this.Mobile_Coloumn, this.Address_Coloumn });
+            this.dataGridView1.Location = new System.Drawing.Point(0, 60);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(519, 615);
+            this.dataGridView1.Size = new System.Drawing.Size(680, 615);
             this.dataGridView1.TabIndex = 0;
             // 
             // ID_Coloumn
@@ -346,7 +342,6 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
-
         }
 
         #endregion
