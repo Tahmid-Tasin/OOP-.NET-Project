f3e89306dcc5bc48ea8ebca53e05c86dbdff058d
Nawar704 <nawaradiba704@gmail.com>
2025-09-08 23:26:55 +0600

    UI redesign and added SPA

--- Store/Store/userinterface/AdminView.Designer.cs
+++ Store/Store/userinterface/AdminView.Designer.cs
@@ -2,34 +2,19 @@ namespace Store
 {
     partial class AdminView
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminView));
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.sideMenuPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.EmployeeBtn = new System.Windows.Forms.Button();
@@ -40,142 +25,94 @@ namespace Store
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2.SuspendLayout();
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.sideMenuPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel2
+            // sideMenuPanel
             // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(76)))), ((int)(((byte)(15)))));
-            this.panel2.AutoScroll = true;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.EmployeeBtn);
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.OutletBtn);
-            this.panel2.Controls.Add(this.button7);
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(-3, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1351, 729);
-            this.panel2.TabIndex = 1;
+            this.sideMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(76)))), ((int)(((byte)(15)))));
+            this.sideMenuPanel.Controls.Add(this.label1);
+            this.sideMenuPanel.Controls.Add(this.button4);
+            this.sideMenuPanel.Controls.Add(this.EmployeeBtn);
+            this.sideMenuPanel.Controls.Add(this.button5);
+            this.sideMenuPanel.Controls.Add(this.OutletBtn);
+            this.sideMenuPanel.Controls.Add(this.button7);
+            this.sideMenuPanel.Controls.Add(this.button6);
+            this.sideMenuPanel.Controls.Add(this.button2);
+            this.sideMenuPanel.Controls.Add(this.button1);
+            this.sideMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.sideMenuPanel.Name = "sideMenuPanel";
+            this.sideMenuPanel.Size = new System.Drawing.Size(200, 729);
+            this.sideMenuPanel.TabIndex = 1;
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 16F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(595, 33);
+            this.label1.Location = new System.Drawing.Point(10, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(195, 47);
+            this.label1.Size = new System.Drawing.Size(180, 40);
             this.label1.TabIndex = 7;
             this.label1.Text = "MENU BAR";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button4
+            // button4 (Dashboard)
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.button4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button4.Location = new System.Drawing.Point(0, 120);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(1351, 87);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "DashBoard";
-            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Text = "Dashboard";
+            this.button4.Height = 50;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // EmployeeBtn
             // 
-            this.EmployeeBtn.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.EmployeeBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.EmployeeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.EmployeeBtn.Location = new System.Drawing.Point(0, 207);
-            this.EmployeeBtn.Name = "EmployeeBtn";
-            this.EmployeeBtn.Size = new System.Drawing.Size(1351, 87);
-            this.EmployeeBtn.TabIndex = 2;
+            this.EmployeeBtn.Dock = System.Windows.Forms.DockStyle.Top;
             this.EmployeeBtn.Text = "Employee";
-            this.EmployeeBtn.UseVisualStyleBackColor = false;
+            this.EmployeeBtn.Height = 50;
             this.EmployeeBtn.Click += new System.EventHandler(this.EmployeeBtn_Click);
             // 
-            // button5
+            // button5 (Stock)
             // 
-            this.button5.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.button5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button5.Location = new System.Drawing.Point(0, 294);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(1351, 87);
-            this.button5.TabIndex = 4;
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
             this.button5.Text = "Stock";
-            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Height = 50;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // OutletBtn (NEW)
+            // OutletBtn
             // 
-            this.OutletBtn.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.OutletBtn.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.OutletBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.OutletBtn.Location = new System.Drawing.Point(0, 381);
-            this.OutletBtn.Name = "OutletBtn";
-            this.OutletBtn.Size = new System.Drawing.Size(1351, 87);
-            this.OutletBtn.TabIndex = 8;
+            this.OutletBtn.Dock = System.Windows.Forms.DockStyle.Top;
             this.OutletBtn.Text = "Outlet";
-            this.OutletBtn.UseVisualStyleBackColor = false;
+            this.OutletBtn.Height = 50;
             this.OutletBtn.Click += new System.EventHandler(this.OutletBtn_Click);
             // 
             // button7 (Products)
             // 
-            this.button7.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.button7.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button7.Location = new System.Drawing.Point(0, 468);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(1351, 87);
-            this.button7.TabIndex = 6;
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
             this.button7.Text = "Products";
-            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Height = 50;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6 (Review)
             // 
-            this.button6.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.button6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button6.Location = new System.Drawing.Point(0, 555);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(1351, 87);
-            this.button6.TabIndex = 5;
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
             this.button6.Text = "Review";
-            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Height = 50;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button2 (VIP Customers)
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
-            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(0, 642);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(1351, 87);
-            this.button2.TabIndex = 1;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
             this.button2.Text = "VIP Customers";
-            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Height = 50;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1 (Logout)
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(197, 164, 133);
             this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(0, 729);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(1351, 87);
-            this.button1.TabIndex = 0;
             this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Height = 50;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
@@ -188,24 +125,31 @@ namespace Store
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // contentPanel
+            // 
+            this.contentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contentPanel.Location = new System.Drawing.Point(200, 0);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(1150, 729);
+            this.contentPanel.TabIndex = 3;
+            // 
             // AdminView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.contentPanel);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.sideMenuPanel);
             this.Name = "AdminView";
             this.Text = "AdminView";
             this.Load += new System.EventHandler(this.AdminView_Load);
-            this.panel2.ResumeLayout(false);
+            this.sideMenuPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-
         }
-
         #endregion
-        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.Panel sideMenuPanel;
+        private System.Windows.Forms.Panel contentPanel;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
--- Store/Store/userinterface/AdminView.cs
+++ Store/Store/userinterface/AdminView.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Windows.Forms;
-
 using Store.userinterface;
 
 namespace Store
@@ -14,49 +13,65 @@ namespace Store
 
         private void AdminView_Load(object sender, EventArgs e)
         {
-
+            // Optionally load a default page, e.g. Dashboard
+            LoadContent(new StockView()); 
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        // Generic loader for child forms inside central panel
+        private void LoadContent(Form childForm)
         {
-            var productView = new ProductManageView();
-            productView.Show();
-            Visible = false;
+            contentPanel.Controls.Clear();
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            contentPanel.Controls.Add(childForm);
+            childForm.Show();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e) // Products
         {
+            LoadContent(new ProductManageView());
+        }
 
+        private void button6_Click(object sender, EventArgs e) // Review
+        {
+            MessageBox.Show("Review module not implemented yet.");
         }
 
         private void EmployeeBtn_Click(object sender, EventArgs e)
         {
-            EmployeeManage emp = new EmployeeManage();
-            emp.Show();
-            Visible = false;
+            LoadContent(new EmployeeManage());
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e) // Stock
         {
-
+            LoadContent(new StockView());
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e) // Dashboard
         {
-            LoginForm loginForm = new LoginForm();
+            MessageBox.Show("Dashboard placeholder.");
+        }
+
+        private void button2_Click(object sender, EventArgs e) // VIP Customers
+        {
+            MessageBox.Show("VIP Customers module not implemented yet.");
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Logout
+        {
+            var loginForm = new LoginForm();
             loginForm.Show();
-            Visible = false;
+            this.Hide();
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
         }
-        
+
         private void OutletBtn_Click(object sender, EventArgs e)
         {
-            var outlet = new OutletManageView();
-            outlet.Show();
-            this.Visible = false;
+            LoadContent(new OutletManageView());
         }
     }
 }
